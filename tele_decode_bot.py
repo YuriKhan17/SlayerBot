559ecaf77d951463b9ddf1869b78a8c77743b141
Yuri <126246603+YuriKhan17@users.noreply.github.com>
2025-05-03 19:47:31 +0100

    Update tele_decode_bot.py

--- tele_decode_bot.py
+++ tele_decode_bot.py
@@ -5,7 +5,7 @@ import time
 import logging
 from decode_tool import decode_file, save_decoded, slash_dump_mode
 
-API_TOKEN = 'API_TOKEN'
+API_TOKEN = 'TELEGRAM_TOKEN'
 ALLOWED_USERS = [6244445306]  # Replace with your Telegram user ID
 
 bot = telebot.TeleBot(API_TOKEN)
